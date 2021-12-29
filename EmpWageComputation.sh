e08d6c78071ea6aef6586a6391c668825963c3b2
srk-333 <sauravmk333@gmail.com>
2021-12-29 10:49:40 +0530

    Added : Emp Attendence Check

--- EmpWageComputation.sh
+++ EmpWageComputation.sh
@@ -1,3 +1,14 @@
 #!/bin/bash -x
 
 echo "Welcome to employee Wage Computation program "
+
+empCheck=$((RANDOM%2));
+
+isPresent=1;
+
+if [[ $isPresent -eq $empCheck ]]
+  then 
+     echo "Emp is present";
+  else
+     echo "Emp is not present";
+fi
